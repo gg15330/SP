9f4bf5a72bc3cfa5671a2ff90086ac0125a8eb57
gg15330 <gg15330@my.bristol.ac.uk>
2016-07-26 16:51:31 +0100

    basic xml generation template

--- DPlib/src/main/java/org/ggraver/DPlib/ClassAnalyser.java
+++ DPlib/src/main/java/org/ggraver/DPlib/ClassAnalyser.java
@@ -12,6 +12,18 @@ import static org.apache.commons.io.FilenameUtils.removeExtension;
 
 import org.ggraver.DPlib.Exception.CompileException;
 import org.ggraver.DPlib.Exception.AnalysisException;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 
 // generates .class files and analyses performance of user solution
@@ -70,6 +82,71 @@ class ClassAnalyser {
         System.out.println("[INSTRUCTIONS] " + instructionCount);
         System.out.println("[TIME] " + executionTime + "ms");
 
+        generateXML();
+
+    }
+
+//    template: sourced from http://www.mkyong.com/java/how-to-create-xml-file-in-java-dom/
+    private void generateXML() {
+        try {
+
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            // root elements
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("company");
+            doc.appendChild(rootElement);
+
+            // staff elements
+            Element staff = doc.createElement("Staff");
+            rootElement.appendChild(staff);
+
+            // set attribute to staff element
+            Attr attr = doc.createAttribute("id");
+            attr.setValue("1");
+            staff.setAttributeNode(attr);
+
+            // shorten way
+            // staff.setAttribute("id", "1");
+
+            // firstname elements
+            Element firstname = doc.createElement("firstname");
+            firstname.appendChild(doc.createTextNode("yong"));
+            staff.appendChild(firstname);
+
+            // lastname elements
+            Element lastname = doc.createElement("lastname");
+            lastname.appendChild(doc.createTextNode("mook kim"));
+            staff.appendChild(lastname);
+
+            // nickname elements
+            Element nickname = doc.createElement("nickname");
+            nickname.appendChild(doc.createTextNode("mkyong"));
+            staff.appendChild(nickname);
+
+            // salary elements
+            Element salary = doc.createElement("salary");
+            salary.appendChild(doc.createTextNode("100000"));
+            staff.appendChild(salary);
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+
+            // Output to console for testing
+             StreamResult result = new StreamResult(System.out);
+
+            transformer.transform(source, result);
+
+            System.out.println("File saved!");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
     }
 
     private long execute(ProcessBuilder pb) throws IOException, InterruptedException {
