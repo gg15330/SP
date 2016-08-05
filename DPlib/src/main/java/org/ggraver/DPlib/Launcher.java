073b40cb8a85bd4a5287ad0cbbecf97c797c5ba9
gg15330 <gg15330@my.bristol.ac.uk>
2016-08-05 13:46:44 +0100

    renamed Main to Launcher

---
+++ DPlib/src/main/java/org/ggraver/DPlib/Launcher.java
@@ -0,0 +1,14 @@
+package org.ggraver.DPlib;
+
+import javafx.application.Application;
+
+// overall program control
+public class Launcher
+
+{
+    public static void main(String args[])
+    {
+        Application.launch(DPlib.class, args);
+    }
+
+}
