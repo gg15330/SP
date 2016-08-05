88cff441fde3b1e3cb3c052ad3fc62cebf504748
gg15330 <gg15330@my.bristol.ac.uk>
2016-08-05 15:46:30 +0100

    Swing test gui

--- DPlib/src/main/java/org/ggraver/DPlib/Launcher.java
+++ DPlib/src/main/java/org/ggraver/DPlib/Launcher.java
@@ -6,34 +6,49 @@ import org.ggraver.DPlib.Exception.ModelingException;
 import java.io.IOException;
 
 import static javafx.application.Application.launch;
+import javax.swing.*;
 
 // overall program control
 public class Launcher
-
 {
-    public static void main(String args[])
-    {
-        try
-        {
-            Class.forName("javafx.application.Application", false, javafx.application.Application.class.getClassLoader());
-            launch(GUIApp.class, args);
-        }
-        catch (ClassNotFoundException cnfe)
-        {
-            System.err.println("Could not launch JavaFX Application - " +
-                                "reverting to command line interface...\n");
-            try
-            {
-                new CommandLineApp().start(args);
-            }
-            catch (IOException | AnalysisException | ModelingException ex)
-            {
-                new IO().exit(ex);
-                System.exit(1);
-            }
-        }
 
+//    public static void main(String args[])
+//    {
+//        launch(GUIApp.class, args);
+//
+//        try
+//        {
+//            new CommandLineApp().start(args);
+//        }
+//        catch (IOException | AnalysisException | ModelingException ex)
+//        {
+//            new IO().exit(ex);
+//            System.exit(1);
+//        }
+//    }
 
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
 }
