6d5cdd76240e8c9930c3f9d64d54482ab3bcfe17
gg15330 <gg15330@my.bristol.ac.uk>
2016-08-06 16:21:19 +0100

    refactored SwingDisplay

--- DPlib/src/main/java/org/ggraver/DPlib/SwingDisplay.java
+++ DPlib/src/main/java/org/ggraver/DPlib/SwingDisplay.java
@@ -8,7 +8,6 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
@@ -20,92 +19,55 @@ class SwingDisplay
 
     void createAndShowGUI()
     {
-        System.out.println("Swing app EDT: " + SwingUtilities.isEventDispatchThread());
-        GridBagConstraints gbc = new GridBagConstraints();
-
-//        editorTextArea
-        JTextArea editor = new JTextArea();
-        editor.setLineWrap(true);
-        editor.setText("Your code here");
-        editor.setBackground(Color.LIGHT_GRAY);
-        JScrollPane editorScrollPane = new JScrollPane(editor);
-        editorScrollPane.setPreferredSize(new Dimension(1, 1));
-        editorScrollPane.setBackground(Color.ORANGE);
-
-//        terminalTextArea
-        JTextArea terminal = new JTextArea();
-        terminal.setLineWrap(true);
-        terminal.setText("Terminal");
-        terminal.setBackground(Color.LIGHT_GRAY);
-        JScrollPane terminalScrollPane = new JScrollPane(terminal);
-        terminalScrollPane.setPreferredSize(new Dimension(1, 1));
-        terminalScrollPane.setBackground(Color.GREEN);
-
-//        ioPanel
-        JPanel ioPanel = new JPanel();
-        ioPanel.setBackground(Color.BLUE);
-        ioPanel.setLayout(new GridBagLayout());
-
-        gbc.gridx = 0;
-        gbc. gridy = 0;
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        ioPanel.add(editorScrollPane, gbc);
-
-        gbc.gridx = 0;
-        gbc. gridy = 1;
-        gbc.weightx = 0;
-        gbc.weighty = 0.5;
-        gbc.fill = GridBagConstraints.BOTH;
-        ioPanel.add(terminalScrollPane, gbc);
+//        text scroll panes
+        JScrollPane editorScrollPane = createTextAreaWithScrollPane("Editor");
+        JScrollPane terminalScrollPane = createTextAreaWithScrollPane("Terminal");
 
 //        button
         JButton solveBtn = new JButton();
         solveBtn.setText("Solve");
 
-        JTextArea executionTimeGraph = new JTextArea();
-        executionTimeGraph.setText("Execution time");
-        executionTimeGraph.setBorder(new EtchedBorder());
-
-        JTextArea instructionCountGraph = new JTextArea();
-        instructionCountGraph.setText("Instruction count");
-        instructionCountGraph.setBorder(new EtchedBorder());
-
 //        executionTimeGraph
-        JFreeChart executionTimeChart = ChartFactory.createBarChart(
-                "Test Chart",
-                "Category",
-                "Score",
-                createDataset(),
-                PlotOrientation.VERTICAL,
-                true, true, false);
-        ChartPanel executionTimeChartPanel = new ChartPanel(executionTimeChart);
-        executionTimeChartPanel.setPreferredSize(new Dimension(200, 300));
+        Dimension chartDimension = new Dimension(200, 300);
+        ChartPanel executionTimeChartPanel = createChart("Execution Time",
+                                                    null,
+                                                    "Time (ms)",
+                                                    createDataset(),
+                                                    PlotOrientation.VERTICAL,
+                                                    chartDimension);
 
 //        instructionCountGraph
-        JFreeChart instructionCountChart = ChartFactory.createBarChart(
-                "Test Chart",
-                "Category",
-                "Score",
-                createDataset(),
-                PlotOrientation.VERTICAL,
-                true, true, false);
-        ChartPanel instructionCountChartPanel = new ChartPanel(instructionCountChart);
-        instructionCountChartPanel.setPreferredSize(new Dimension(200, 300));
+        ChartPanel instructionCountChartPanel = createChart("Instruction",
+                                                            null,
+                                                            "Instructions executed",
+                                                            createDataset(),
+                                                            PlotOrientation.VERTICAL,
+                                                            chartDimension);
+
+//        ioPanel
+        JPanel ioPanel = createIOPanel(editorScrollPane, terminalScrollPane);
 
 //        graphPanel
-        JPanel graphPanel = new JPanel();
-        BoxLayout graphPanelLayout = new BoxLayout(graphPanel, BoxLayout.Y_AXIS);
-        graphPanel.setLayout(graphPanelLayout);
-        graphPanel.add(solveBtn);
-        graphPanel.add(executionTimeChartPanel);
-        graphPanel.add(instructionCountChartPanel);
+        JPanel graphPanel = createGraphPanel(solveBtn, executionTimeChartPanel, instructionCountChartPanel);
 
 //        mainPanel
+        JPanel mainPanel = createMainPanel(ioPanel, graphPanel);
+
+        JFrame frame = new JFrame("Test gui");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+        frame.setContentPane(mainPanel);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private JPanel createMainPanel(JPanel ioPanel, JPanel graphPanel)
+    {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
-        mainPanel.setBackground(Color.GRAY);
+        GridBagConstraints gbc = new GridBagConstraints();
 
         gbc.gridx = 0;
         gbc. gridy = 0;
@@ -120,44 +82,83 @@ class SwingDisplay
         gbc.weighty = 1;
         gbc.fill = GridBagConstraints.BOTH;
         mainPanel.add(graphPanel, gbc);
-
-        JFrame frame = new JFrame("Test gui");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(800, 600));
-        frame.setContentPane(mainPanel);
-        frame.setResizable(false);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        return mainPanel;
     }
 
-    private CategoryDataset createDataset( )
+    private JPanel createGraphPanel(JButton solveBtn, JPanel executionTimeChartPanel, JPanel instructionCountChartPanel)
     {
-        final String fiat = "FIAT";
-        final String audi = "AUDI";
-        final String ford = "FORD";
-        final String speed = "Speed";
-        final String millage = "Millage";
-        final String userrating = "User Rating";
-        final String safety = "safety";
-        final DefaultCategoryDataset dataset =
-                new DefaultCategoryDataset( );
+        JPanel graphPanel = new JPanel();
+        BoxLayout graphPanelLayout = new BoxLayout(graphPanel, BoxLayout.Y_AXIS);
+        graphPanel.setLayout(graphPanelLayout);
+        graphPanel.setBorder(new EtchedBorder());
+        graphPanel.add(solveBtn);
+        graphPanel.add(executionTimeChartPanel);
+        graphPanel.add(instructionCountChartPanel);
+        return graphPanel;
+    }
 
-        dataset.addValue( 1.0 , fiat , speed );
-        dataset.addValue( 3.0 , fiat , userrating );
-        dataset.addValue( 5.0 , fiat , millage );
-        dataset.addValue( 5.0 , fiat , safety );
+    private JPanel createIOPanel(JScrollPane editorScrollPane, JScrollPane terminalScrollPane)
+    {
+        JPanel ioPanel = new JPanel();
+        ioPanel.setLayout(new GridBagLayout());
+        ioPanel.setBorder(new EtchedBorder());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc. gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        ioPanel.add(editorScrollPane, gbc);
 
-        dataset.addValue( 5.0 , audi , speed );
-        dataset.addValue( 6.0 , audi , userrating );
-        dataset.addValue( 10.0 , audi , millage );
-        dataset.addValue( 4.0 , audi , safety );
+        gbc.gridx = 0;
+        gbc. gridy = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0.5;
+        gbc.fill = GridBagConstraints.BOTH;
+        ioPanel.add(terminalScrollPane, gbc);
+        return ioPanel;
+    }
 
-        dataset.addValue( 4.0 , ford , speed );
-        dataset.addValue( 2.0 , ford , userrating );
-        dataset.addValue( 3.0 , ford , millage );
-        dataset.addValue( 6.0 , ford , safety );
+    private ChartPanel createChart(String name,
+                                   String xLabel,
+                                   String yLabel,
+                                   CategoryDataset dataSet,
+                                   PlotOrientation orientation,
+                                   Dimension size)
+    {
+        JFreeChart jFreeChart = ChartFactory.createBarChart(
+                name,
+                xLabel,
+                yLabel,
+                dataSet,
+                orientation,
+                true,
+                true,
+                false);
+        ChartPanel chartPanel = new ChartPanel(jFreeChart);
+        chartPanel.setPreferredSize(size);
+        return chartPanel;
+    }
 
+    private JScrollPane createTextAreaWithScrollPane(String text)
+    {
+        JTextArea jTextArea = new JTextArea();
+        jTextArea.setLineWrap(true);
+        jTextArea.setText(text);
+        JScrollPane scrollPane = new JScrollPane(jTextArea);
+        scrollPane.setPreferredSize(new Dimension(1, 1));
+        return scrollPane;
+    }
+
+
+    private CategoryDataset createDataset()
+    {
+        final String model = "Model";
+        final String user = "User";
+        final String value = "";
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(150000000 , model, value);
+        dataset.addValue(140000000 , user , value);
         return dataset;
     }
 
