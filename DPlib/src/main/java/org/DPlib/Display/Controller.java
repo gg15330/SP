5d1387613363dfd1e97c0e3747ffb056b9d899e5
gg15330 <gg15330@my.bristol.ac.uk>
2016-08-15 11:51:09 +0100

    fixed exception handling in Solver done() method

---
+++ DPlib/src/main/java/org/DPlib/Display/Controller.java
@@ -0,0 +1,75 @@
+package org.DPlib.Display;
+
+import org.DPlib.*;
+import org.DPlib.Exception.AnalysisException;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by george on 07/08/16.
+ */
+public class Controller
+{
+
+    private final IO io = new IO();
+    private FileHandler fileHandler;
+    private View view = new View();
+    private Model model;
+
+    public Controller(String filePath)
+    throws IOException
+    {
+        view.addSolveBtnListener(new solveBtnListener());
+        this.fileHandler = new FileHandler(filePath, "mod");
+    }
+
+    public void start()
+    throws IOException, AnalysisException
+    {
+        model = fileHandler.deserializeModelFile();
+        DefaultCategoryDataset tutorTimeDataset = new DefaultCategoryDataset();
+        DefaultCategoryDataset tutorOutputDataset = new DefaultCategoryDataset();
+
+        for(int i = 0; i < model.getResults().size(); i++)
+        {
+            Result r = model.getResults().get(i);
+            tutorTimeDataset.addValue(r.getExecutionTime(), "Tutor", String.valueOf(i));
+            tutorOutputDataset.addValue(Long.parseLong(r.getOutput()), "Tutor", String.valueOf(i));
+        }
+
+        view.setTutorTimeData(tutorTimeDataset);
+        view.setTutorOutputData(tutorOutputDataset);
+        view.setEditorText(new CodeGenerator().generate(model.getClassName(),
+                                                        model.getCallingMethodDeclaration(),
+                                                        model.getCallingMethodBody(),
+                                                        model.getMethodToAnalyseDeclaration()));
+
+        SwingUtilities.invokeLater(view::createAndShowGUI);
+    }
+
+    private class solveBtnListener
+    implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            try
+            {
+                File javaFile = fileHandler.createJavaFile(view.getEditorText());
+                new Solver(model, javaFile, view, io).execute();
+            }
+            catch (IOException ioe)
+            {
+                io.errorMsg(ioe);
+            }
+        }
+
+    }
+
+}
---
+++ DPlib/src/main/java/org/DPlib/IO.java
@@ -0,0 +1,124 @@
+package org.DPlib;
+
+import org.DPlib.Exception.AnalysisException;
+import org.DPlib.Exception.ModelingException;
+
+import java.io.IOException;
+
+// IO handling
+public class IO
+{
+
+    private String command;
+    private String filePath;
+    private String methodName;
+
+    public void errorMsg(Throwable t)
+    {
+        if (!(t instanceof AnalysisException)
+                && !(t instanceof ModelingException)
+                && !(t instanceof IOException))
+        {
+            t.printStackTrace();
+            throw new Error("Expected ModelingException, AnalysisException or IOException.");
+        }
+        System.err.println(t.getMessage() + "\n");
+    }
+
+    void usage()
+    {
+        System.err.println(usage);
+    }
+
+//    void displayResult(Result result)
+//    {
+//        String[][] resultsTable = new String[4][4];
+//
+//        resultsTable[0] = new String[] {"", "MODEL", "USER", "RESULT"};
+//        resultsTable[1] = new String[] {
+//                "OUTPUT",
+//                result.getModelOutput(),
+//                result.getUserOutput(),
+//                pass(result.getOutputPass())
+//        };
+//        resultsTable[2] = new String[] {
+//                "EXECUTION TIME",
+//                String.valueOf(result.getModelExecutionTime()),
+//                String.valueOf(result.getUserExecutionTime()),
+//                pass(result.getExecutionTimePass())
+//        };
+//        resultsTable[3] = new String[] {
+//                "INSTRUCTION COUNT",
+//                String.valueOf(result.getModelInstructionCount()),
+//                String.valueOf(result.getUserInstructionCount()),
+//                pass(result.getInstructionCountPass())
+//        };
+//
+//        System.out.println();
+//        for(String[] row : resultsTable)
+//        {
+//            System.out.format("%-20s%-20s%-20s%-20s\n", row[0], row[1], row[2], row[3]);
+//        }
+//        System.out.println();
+//    }
+
+    private String pass(boolean b)
+    {
+        if(b)
+        {
+            return "PASS";
+        }
+        else
+        {
+            return "FAIL";
+        }
+    }
+
+    String getMethodName()
+    {
+        return methodName;
+    }
+
+    public String getFilePath()
+    {
+        return filePath;
+    }
+
+    private String usage = "\nUsage:\n" +
+            "\nTo model a problem:\n" +
+            "\njava -jar DPLib-1.0-SNAPSHOT.jar " +
+            "model <path/to/file_to_model.java> <method_to_analyse>\n" +
+            "\nTo solve:\n" +
+            "\njava -jar DPLib-1.0-SNAPSHOT.jar " +
+            "<path/to/file_to_solve.java>\n";
+
+    private String analysisReport;
+
+
+    String getCommand()
+    {
+        return command;
+    }
+
+    public void printResult(boolean pass)
+    {
+        if(pass)
+        {
+        }
+        else
+        {
+        }
+
+    }
+
+    public void fail(String userOutput, String tutorOutput)
+    {
+        System.out.println("\nFAIL: output \"" + userOutput + "\"" +
+                           "does not match expected output \"" + tutorOutput + "\".\n");
+    }
+
+    public void pass()
+    {
+        System.out.println("\nPASS\n");
+    }
+}
