cb8d27f780fac43988f887515d5d18e80a4a0656
gg15330 <gg15330@my.bristol.ac.uk>
2016-08-17 18:11:39 +0100

    trying to format String

--- DPlib/src/main/java/org/dplib/CodeGenerator.java
+++ DPlib/src/main/java/org/dplib/CodeGenerator.java
@@ -5,6 +5,8 @@ package org.dplib;
  */
 public class CodeGenerator
 {
+    private final String indentString = "||||";
+
     public String generate(String className,
                            String callingMethodDeclaration,
                            String callingMethodBody,
@@ -27,8 +29,23 @@ public class CodeGenerator
 
     private String createMethod(String declaration, String body)
     {
+        int indent = 0;
         StringBuilder sb = new StringBuilder();
-        sb.append(declaration + body);
+        sb.append(declaration + "\n");
+        for(int j = 0; j < indent; j++) { sb.append(indentString); }
+        for(int i = 2; i < body.length(); i++)
+        {
+            if(body.charAt(i-1) == '\n'
+            && body.charAt(i-2) == '{')
+            {
+                for(int j = 0; j < ++indent; j++) { sb.append(indentString); }
+            }
+            sb.append(body.charAt(i));
+            if(body.charAt(i-1) == '{') { indent++; }
+            if(body.charAt(i-1) == '}') { indent--; }
+        }
+        System.out.println(body);
+        System.out.println(sb.toString());
         return sb.toString();
     }
 
